b37aecd9469e90a63ef22711a8cd67dcafcb3056
danielipark <51270111+danielipark@users.noreply.github.com>
2020-04-15 21:53:15 -0400

    Delete types.js

--- refactoring/types.js
+++
@@ -1,3 +0,0 @@
-var typed = new Typed(".ghosttype", {
-    strings: ["Perfecting my jumpshot.^250", "Singing my heart out in the shower.^250", "Playing endless boardgames...specifically Avalon.^250", "Meal prepping chicken breast and broccoli...sigh."], typeSpeed: 50, startDelay: 2500, showCursor: false, backSpeed: 50, backDelay: 5000, loop: true
-  });
