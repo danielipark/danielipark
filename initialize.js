f01b2acb3f888a7c6069543e9ed3c9e43fe16775
Daniel Park <51270111+danielipark@users.noreply.github.com>
2020-04-25 00:46:40 -0400

    Add files via upload

--- initialize.js
+++ initialize.js
@@ -1,10 +1,8 @@
-AOS.init(
-    {
-      once: true
-    }
-  );
+AOS.init({
+  once: true
+});
 
-$(".navbar-burger").click(function() {
+$(".navbar-burger").click(function () {
   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
   $(".navbar-burger").toggleClass("is-active");
   $(".navbar-end").toggleClass("has-text-centered");
--- types.js
+++ types.js
@@ -1,3 +1,9 @@
 var typed = new Typed(".ghosttype", {
-    strings: ["Perfecting my jumpshot.^250", "Making travel videos.^250", "Singing my heart out in the shower.^250", "Playing endless board games...specifically Avalon.^250", "Meal prepping chicken breast and broccoli...sigh."], typeSpeed: 50, startDelay: 2500, showCursor: false, backSpeed: 50, backDelay: 5000, loop: true
-  });
+  strings: ["Perfecting my jumpshot.^250", "Making travel videos.^250", "Singing my heart out in the shower.^250", "Playing endless board games...specifically Avalon.^250", "Meal prepping chicken breast and broccoli...sigh."],
+  typeSpeed: 50,
+  startDelay: 2500,
+  showCursor: false,
+  backSpeed: 50,
+  backDelay: 5000,
+  loop: true
+});
