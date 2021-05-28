4b4cbd5c7ecff418b8ef362c3d53a719c61a5638
Daniel Park <51270111+danielipark@users.noreply.github.com>
2021-05-28 00:29:23 -0400

    Add files via upload

---
+++ types-audi.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".ghosttype-audi", {
+  strings: ["\"How can I trust this used car?\"^250", "\"Is there a special offer?\"^250", "\"What’s the limited warranty?\"^250"],
+  typeSpeed: 25,
+  startDelay: 1000,
+  showCursor: false,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true
+});
--- types.js
+++ types.js
@@ -1,9 +1,9 @@
 var typed = new Typed(".ghosttype", {
   strings: ["I only cried twice during the pandemic.^250", "you can find me absolutely crushing it in my church basketball league.^250", "I enjoy playing Avalon for hours.^250", "I can eat shawarma once a week...okay maybe twice.^250", "I enjoy singing songs by Akon."],
-  typeSpeed: 50,
+  typeSpeed: 25,
   startDelay: 1000,
   showCursor: false,
-  backSpeed: 50,
+  backSpeed: 20,
   backDelay: 1000,
   loop: true
 });
